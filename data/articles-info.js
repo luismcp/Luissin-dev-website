44c265354150dcbb1fe63c010c52889311886427
Luismcp <luismcp_1@hotmail.com>
2026-02-16 16:25:59 -0600

    feat: improve page loading and image accessibility

--- data/articles-info.js
+++ data/articles-info.js
@@ -3,24 +3,24 @@ const articles = [
     id: 1,
     title: 'Análisis del desarrollo de productos notables y factorizacion',
     description: 'Una diferente perspectica con objetivo a la comprensión y desarrollo de agilidad matemática y mental, favorecedora de la elaboración de estrategias de resolución de problemas matemáticos, a parti de la intuición, y no de la memorización.',
-    img_url: '/images/lib-page.JPG',
-    img_alt: 'AI image',
+    img_url: '/images/factorizacion-cover.jpg',
+    img_alt: 'Foto del libro de Matematicas Cuarto de Santallo-Carbonel con ejercicios varios',
     href: '/factorizacion.html'
   },
   {
     id: 2,
     title: 'Deducción de las propiedades de los exponentes y radicales',
     description: 'Las propiedades algebraicas, aunque muy útiles, no son leyes inquebrantables. En este artículo, se tiene por objetivo mostrar deducciones (no demostraciones) algebraicas de las propiedades de los exponentes y radicales, para así, en determinado momento, ser capaces de responder a la pregunta: ¿Puedo aplicar aquí esta propiedad?.',
-    img_url: '/images/exponenciacion-cover.jpeg',
-    img_alt: 'remote work image',
+    img_url: '/images/exponenciacion-cover.jpg',
+    img_alt: 'Foto tomada a sesión de estudio del libro de Matematicas Cuarto de Santallo-Carbonel',
     href: '/exponentes-y-radicales.html'
   },
   {
     id: 3,
     title: 'Deducción de las leyes de logaritmos',
     description: 'La ciencia, la tecnologia, la economia y la ingenieria son solo algunos de los ejemplos de la práctica de los logaritmos. Si la vida ya es bella, comprender qué es un logaritmo y de donde provienen sus cualidades, despertará en nosotros una nueva perspectiva de la realidad, una nueva forma de entender el mundo que nos rodea, y una nueva forma de vernos a nosotros mismos.',
-    img_url: '/images/log-cover.jpeg',
-    img_alt: 'mental health image',
+    img_url: '/images/logaritmos-cover.jpg',
+    img_alt: 'Foto del libro de Matematicas Cuarto de Santallo-Carbonel con formulas de logaritmos',
     href: '/leyes-logaritmos.html'
   }
 ]
